5625cd63abe5f2cd0749b6ad81fccd3668c93ba2
antish24 <aantenehh@gmail.com>
2024-06-25 05:55:45 -0700

    marrvellous 5nd

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from './footer.module.css'
 import { NavLink } from 'react-router-dom'
-import logo from '../../assets/logo.png'
+import logo from '../../assets/logopng.png'
 import { FaFacebook, FaInstagram, FaLinkedin, FaTelegram, FaTiktok } from 'react-icons/fa'
 
 const Footer = () => {
